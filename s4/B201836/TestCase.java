80f308ab4861346c308401afba661f51dfbf295a
tut201836 <120451665+tut201836@users.noreply.github.com>
2022-12-20 17:34:52 +0900

    Update TestCase.java

--- s4/B201836/TestCase.java
+++ s4/B201836/TestCase.java
@@ -46,7 +46,10 @@ public class TestCase {
 	    assert freq == 4: "Hi Ho Hi Ho, H: " + freq;
 	    // Write your testCase here
 	    System.out.println("checking my Frequencer");
-	    
+	    myObject.setSpace("Hi Ho Hi Ho oH".getBytes());
+	    myObject.setTarget("H".getBytes());
+	    freq = myObject.frequency();
+	    assert freq == 5: "Hi Ho Hi Ho, H: " + freq;
 
 	}
 	catch(Exception e) {
